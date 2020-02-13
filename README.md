f4d4578c761cf65147573a73e0de0877ed68dcea
DariusRain <dariusrain.student@careerdevs.com>
2020-02-12 19:29:23 -0500

    Added simple validation with Joi

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
     1. Set up routes and install express then mongoose.
     2. Adding a User schema with mongoose and export it as a model.
+    3. Validate information from the request using Joi. ( Joi Schemas & schema.validate() )
--- routes/auth.js
+++ routes/auth.js
@@ -1,27 +1,51 @@
-const router = require('express').Router();
-const User = require('../model/User')
+const router = require("express").Router();
+const User = require("../model/User");
+const Joi = require("@hapi/joi");
 
+
+//Joi schema
+const schema = Joi.object({
+  name: Joi.string() //These methods are similar to mongoose 
+    .min(6) 
+    .required(),
+  email: Joi.string()
+    .min(6)
+    .required()
+    .email(),
+  password: Joi.string()
+    .min(6)
+    .required()
+});
 
 //Add async for any function fetching data.
-router.post('/register', async (req, res) => {
-    const user =  new User( {
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    })
-//User.save() returns a promise using try and catch to consume 
-// the promise value of either resolve or reject
-// are similar to using .then()
+router.post("/register", async (req, res) => {
 
-    try {
-        const savedUser = await user.save() 
-        res.status(201).send(savedUser) //Resolved
-    }
-    catch (err) {
-        res.status(401).send(err) //Rejected
-    }
+    //Validation with  Joi
 
+    //Without destructuring
+    // const validation = schema.validate(req.body);
+    
+    //Object destructuring
+    //To get the error message properly
+    const { error } = schema.validate(req.body)
+
+    
+
+
+    res.send(error.details[0].message)
 })
+//   const user = new User({
+//     name: req.body.name,
+//     email: req.body.email,
+//     password: req.body.password
+//   });
 
+//   try {
+//     const savedUser = await user.save();
+//     res.status(201).send(savedUser); //Resolved
+//   } catch (err) {
+//     res.status(401).send(err); //Rejected
+//   }
+// });
 
 module.exports = router;
