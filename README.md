8c9b97f53d434b11559428947129e21dd550eced
DariusRain <dariusrain.student@careerdevs.com>
2020-02-12 19:36:18 -0500

    Validated with Joi before request to the database

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
     1. Set up routes and install express then mongoose.
     2. Adding a User schema with mongoose and export it as a model.
     3. Validate information from the request using Joi. ( Joi Schemas & schema.validate() )
+    4. Use the validation from Joi before a post gets saved to the database.
--- routes/auth.js
+++ routes/auth.js
@@ -17,35 +17,25 @@ const schema = Joi.object({
     .required()
 });
 
-//Add async for any function fetching data.
+
 router.post("/register", async (req, res) => {
-
-    //Validation with  Joi
-
-    //Without destructuring
-    // const validation = schema.validate(req.body);
-    
-    //Object destructuring
-    //To get the error message properly
     const { error } = schema.validate(req.body)
 
-    
+    if(error) return res.status(400).send(error.details[0].message)
 
 
-    res.send(error.details[0].message)
-})
-//   const user = new User({
-//     name: req.body.name,
-//     email: req.body.email,
-//     password: req.body.password
-//   });
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password
+  });
 
-//   try {
-//     const savedUser = await user.save();
-//     res.status(201).send(savedUser); //Resolved
-//   } catch (err) {
-//     res.status(401).send(err); //Rejected
-//   }
-// });
+  try {
+    const savedUser = await user.save();
+    res.status(201).send(savedUser); //Resolved
+  } catch (err) {
+    res.status(401).send(err); //Rejected
+  }
+});
 
 module.exports = router;
